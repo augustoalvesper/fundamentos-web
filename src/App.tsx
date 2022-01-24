4318203bce47ac79ab70c046bb723c53298c7210
augustoalvesper <93775760+augustoalvesper@users.noreply.github.com>
2022-01-24 09:59:31 -0300

    Adicionando Get All

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listaTema/ListaTema';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
+
+            <Route exact path='/'>
+              <Login />
+            </Route>
+
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+
+            <Route path='/cadastrousuario'>
+              <CadastroUsuario />
+            </Route>
+            <Route path='/temas'>
+              <ListaTema />
+            </Route>
+            <Route path='/posts'>
+              <ListaPostagem />
+            </Route>
+          </div>
+        </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
